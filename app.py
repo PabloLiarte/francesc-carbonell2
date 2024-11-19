e9089cfff2f2f1ed39728da4ba7e2a2a9d2681d2
PabloLiarte <pablo.liarteg@gmail.com>
2024-11-19 14:10:00 +0100

    test1

--- app.py
+++ app.py
@@ -13,7 +13,13 @@ app.secret_key = 'your_secret_key'  # Cambia esto por una clave segura
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    image_files = os.listdir(os.path.join(app.static_folder, 'uploads'))
+    image_files.sort()  # Opcional: ordenar los archivos
+    image_urls = [
+        url_for('static', filename=f'uploads/{filename}')
+        for filename in image_files if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))
+    ]
+    return render_template('index.html', images=image_urls)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
